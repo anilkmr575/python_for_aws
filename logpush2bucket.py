ef7de96174f1f3e29c6a5af4ef82130338e9b2ba
anilkmr575 <anilkmr575@gmail.com>
2018-07-26 18:38:34 +0530

    Update logpush2bucket.py

--- logpush2bucket.py
+++ logpush2bucket.py
@@ -3,8 +3,8 @@ import boto.s3
 import sys
 from boto.s3.key import Key
 
-AWS_ACCESS_KEY_ID = 'AKIAIPU32PYEYHRUDDUQ'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
 
 bucket_name = AWS_ACCESS_KEY_ID.lower() + 'devopser2'
 conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
